50301f900409802de504bf4a2e7a5b3ae66590c7
Andrew <awebb395#yahoo.com>
2023-01-18 10:19:17 -0600

    added logincontroller and login jsp

---
+++ src/main/java/com/webb/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,20 @@
+package com.webb.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+	
+	@RequestMapping("/login")
+	public String goToLoginPage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		System.out.println(name);
+		return "login";
+		
+	}
+	
+}
